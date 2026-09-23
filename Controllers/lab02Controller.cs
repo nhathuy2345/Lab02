d776c6f649b983004d153c2357d86f54fb0e52bd
ADMIN <ADMIN@DESKTOP-F09OLV9>
2026-09-23 10:44:24 +0700

    Add project files.

---
+++ Controllers/lab02Controller.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace lab02.Controllers
+{
+    public class lab02Controller : Controller
+    {
+        public ActionResult bai2()
+        {
+            return View();
+        }
+        public ActionResult bai1()
+        {
+            return View();
+        }
+
+        public ActionResult vd5()
+        {
+            return View();
+        }
+
+        public ActionResult vd4()
+        {
+            return View();
+        }
+
+
+        public ActionResult vd3()
+        {
+            return View();
+        }
+        public ActionResult vd2()
+        {
+            return View();
+        }
+        public ActionResult vidu1()
+        {
+            return View();
+        }
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
